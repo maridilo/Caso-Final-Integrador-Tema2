752366f5f7bbe1b8120401eb6753c71f23e71eee
maridilo <146822054+maridilo@users.noreply.github.com>
2024-03-10 17:51:18 +0100

    commit

--- SistemadeGestionparaZoologicoInteligente/src/main/java/CuidadoAnimales/Ave.java
+++ SistemadeGestionparaZoologicoInteligente/src/main/java/CuidadoAnimales/Ave.java
@@ -1,4 +1,20 @@
 package CuidadoAnimales;
 
-public class Ave {
-}
+    public class Ave extends Animal {
+        private int envergaduraAlas;
+
+        public Ave(String nombre, int edad, int envergaduraAlas) {
+            super(nombre, edad);
+            this.envergaduraAlas = envergaduraAlas;
+        }
+
+        // Métodos getter y setter específicos
+        public int getEnvergaduraAlas() {
+            return envergaduraAlas;
+        }
+
+        public void setEnvergaduraAlas(int envergaduraAlas) {
+            this.envergaduraAlas = envergaduraAlas;
+        }
+    }
+
--- SistemadeGestionparaZoologicoInteligente/src/main/java/CuidadoAnimales/Reptil.java
+++ SistemadeGestionparaZoologicoInteligente/src/main/java/CuidadoAnimales/Reptil.java
@@ -1,5 +1,20 @@
 package CuidadoAnimales;
 
-public class Reptil {
+    public class Reptil extends Animal {
+        private boolean venenoso;
 
-}
+        public Reptil(String nombre, int edad, boolean venenoso) {
+            super(nombre, edad);
+            this.venenoso = venenoso;
+        }
+
+        // Métodos getter y setter específicos
+        public boolean esVenenoso() {
+            return venenoso;
+        }
+
+        public void setVenenoso(boolean venenoso) {
+            this.venenoso = venenoso;
+        }
+    }
+
